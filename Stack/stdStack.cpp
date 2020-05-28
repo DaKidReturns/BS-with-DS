632449cac8fab5139b82b5ecb2ba8cf6c6b9eee3
Rohit Karunakaran <rohitkarun@gmail.com>
2020-05-28 22:55:08 +0530

    On branch master  Changes to be committed: 	modified:   stdStack.cpp

--- Stack/stdStack.cpp
+++ Stack/stdStack.cpp
@@ -1,7 +1,38 @@
-#include<iostream>
+#include<cstdio>
 #include<stack>
 
 int main(){
     std::stack<int> mystack;
-
+    int ch;
+    int elem;
+    bool RUN = true;
+    printf("STACK IMPLEMENTATION USING THE STANDARD LIBRARY FUNCTIONSN\n");    
+    while(RUN){
+        printf("-------Menu-------");
+        printf("\n1.Insert\n");
+        printf("2.Pop\n");
+        printf("3.Is Empyt\n");
+        printf("4.Top");
+        printf("\nEnter Choice---> ");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1:printf("\nEnter the element to be inserted "); 
+                   scanf("%d",&elem);
+                   mystack.push(elem);
+                   RUN = false;
+                   printf("%d",mystack.top());
+                   break;
+            case 2:printf("Element poped is = %d\n",mystack.top());
+                   mystack.pop();
+                   break;
+            case 3:if(mystack.empty()){
+                        printf("Stack is empty\n");
+                   }
+                   else{
+                        printf("Stack ain't empty\n");   
+                   }
+                   break;
+            case 4:;
+        }
+    }
 }
