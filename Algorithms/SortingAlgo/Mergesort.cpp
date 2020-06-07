11f4255b78bf2c946c5f1e44e87fc38ba38690ba
Rohit Karunakaran <rohitkarun@gmail.com>
2020-06-07 13:29:00 +0530

    Changes to be committed: 	modified:   Mergesort.cpp
     Changes not staged for commit:
    	modified:   Mergesort.cpp

--- Algorithms/SortingAlgo/Mergesort.cpp
+++ Algorithms/SortingAlgo/Mergesort.cpp
@@ -5,11 +5,12 @@ void fingerThing(std::vector<int>& arr,int last,int begin,int mid);
 void mergeSort(std::vector<int>& arr,int begin, int last){ 
     //std::cout<<"In merge sort"<<'\n';
     int mid; 
+    mid = (begin+last)/2;
     if(begin<last){
-      mid = (begin+last)/2;
+      //mid = (begin+last)/2;
       mergeSort(arr,begin,mid);
-      mergeSort(arr,mid,last-1);
-      fingerThing(arr,last,begin,mid);
+      mergeSort(arr,mid+1,last);
+      fingerThing(arr,begin,mid,last);
     } 
     ITERCOUNT++;
     std::cout<<"Iteration "<<ITERCOUNT;
@@ -17,20 +18,20 @@ void mergeSort(std::vector<int>& arr,int begin, int last){
     std::cout<<"Value of begin = "<<begin<<'\n';
     std::cout<<"Value of last = "<<last<<'\n';
     std::cout<<"Value of mid = "<<mid<<'\n';
-    for(int i = 0;i<arr.size();i++){
+    for(int i = begin;i<=last;i++){
         std::cout<<arr[i]<<' ';
     }
     std::cout<<"\n\n";
     return;
 }
-void fingerThing(std::vector<int>& arr ,int last,int begin,int mid){
+void fingerThing(std::vector<int>& arr ,int begin,int mid,int last){
     //std::cout<<"In pointer sort \n";
     if(last==begin){
         return; 
     }
-    int b=begin,c=mid;
+    int b=begin,c=mid+1;
     std::vector<int> temp;
-    while(b<mid&&c<last){
+    while(b<=mid&&c<=last){
         if(arr[b]<arr[c]){
             temp.push_back(arr[b]);
             b++;
@@ -40,8 +41,8 @@ void fingerThing(std::vector<int>& arr ,int last,int begin,int mid){
             c++;
         }
     }
-    while(b<mid){temp.push_back(arr[b++]);}
-    while(c<last){temp.push_back(arr[c++]);}
+    while(b<=mid){temp.push_back(arr[b++]);}
+    while(c<=last){temp.push_back(arr[c++]);}
     for(int i = 0;i<temp.size();i++){
         arr[begin+i] = temp[i];
     }
@@ -52,12 +53,12 @@ void fingerThing(std::vector<int>& arr ,int last,int begin,int mid){
 
 
 int main(){
-    std::vector<int> spy({7,4,1,5,9,6,2,8,3});   
+    std::vector<int> spy({7,4,3,8,0,2,4,3,2});   
     for(auto it=spy.begin();it!=spy.end();++it){
         std::cout<<*it<<' '; 
     }
     std::cout<<'\n';
-   mergeSort(spy,0,spy.size());
+   mergeSort(spy,0,spy.size()-1);
     for(auto it=spy.begin();it!=spy.end();++it){
         std::cout<<*it<<' '; 
     }
