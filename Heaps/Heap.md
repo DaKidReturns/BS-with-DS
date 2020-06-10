c64d330736ee3de8a03eb55d42a016e8f0840d9e
Rohit Karunakaran <rohitkarun@gmail.com>
2020-06-10 18:07:19 +0530

    Changes to be committed: 	modified:   Heap.md 	modified:   MaxHeap.cpp
     Changes not staged for commit:
    	modified:   ../Algorithms/SortingAlgo/Mergesort.cpp
    	modified:   MinHeap.cpp
    
     Untracked files:
    	../Algorithms/SortingAlgo/hs.cpp

--- Heaps/Heap.md
+++ Heaps/Heap.md
@@ -3,3 +3,28 @@ Heaps
 Heaps are a beautiful liitle data structures.  
 Formally heaps are defined as: A ***nearly complete*** binary tree.  
 It is similar to a priority queue
+
+### Max heap and Min heap
+*Max Heap* : The children of a node is ***smaller*** than the parent node(if there are children)  
+*Min Heap* : The children of a node is ***larger*** than the parent node (if there are children)
+
+### Basic Operations on Heaps
+1. `find_max` or `find_min` : Find the maximum(in case of a max heap) or 
+    minimum (in case of a min heap) of a min heap ie the root node.
+2. `insert` : Insert a new key into the heap.  
+3.  `extract_max` : remove the largest element from the heap.  
+4.  `build heap`: creates a heap out of an array  
+5.  `heapify` : creates a min/max heap out of a given node, assuming that the chidren are min/max 
+     heaps  
+6.  `isEmpty` : Checks if a heap is empty or not.
+7.  `size` : returns the size of a heap.
+
+### Varients of heaps
+There are many varients of heap such as a Fibonacci heap, binomial heap, Radix heap, etc.  
+
+### References
+For more info on heap refer:  
+[Wikipedia][link1]
+
+
+[link1]:https://en.wikipedia.org/wiki/Heap_%28data_structure%29
--- Heaps/MaxHeap.cpp
+++ Heaps/MaxHeap.cpp
@@ -1,58 +1,110 @@
 #include <iostream>
 #include <vector>
 
-void swap(std::vector<int>& arr, int i,int j){
+class MaxHeap{
+    private:
+        std::vector<int> arr;
+        int size;
+        int max;
+        void swap(int i, int j);
+        void maxHeapify(int i);
+        void buildMaxHeap();
+    public:
+        MaxHeap();
+        int findMax() const;
+        int extractMax();
+        void insert(int& a); 
+        const bool isEmpty() const;
+        void printHeap() const;
+};
+void MaxHeap::swap(int i,int j){
     int temp=arr[i];
     arr[i]=arr[j];
     arr[j]=temp;
 }
 
-void heapify(std::vector<int>& arr,int i,int n){
+void MaxHeap::maxHeapify(int i){
     //assumes that the children are max heaps
     //check if the parent is less than the child
     //if not swap the  parent with the  child
-    int leftChild,rightChild,largestChild;
-    if(i<=n/2){
-        //The above condition checks that the node checked has atleast
-        //one child 
-        leftChild=2*i+1;
-        rightChild=2*i+2;
-        //there will be one or two childeren
-        if(n<=rightChild){
-            //there is no right child
-            //in that case the largest child is the left child
-            largestChild=leftChild;
-        }
-        else{
-            largestChild = arr[leftChild]<arr[rightChild]?rightChild:leftChild;
-        }
-        if(arr[largestChild]>arr[i]){
-            //if the largest child is greater than the parent then swap them
-            swap(arr,i,largestChild);
-            heapify(arr,largestChild,n);
-        }
+    int leftChild,rightChild,largest;
+    leftChild=2*i+1;
+    rightChild=2*i+2;
+    largest= i;
+    //there will be one or two childeren we will check for the chidern in the
+    //below if statements
+    if(size>leftChild && arr[leftChild]>arr[largest])largest = leftChild;
+    if(size>rightChild && arr[rightChild]>arr[largest])largest = rightChild;
+    //If i is not the largest then swap them
+    if(largest != i){
+        //if the largest is not the parent than one of the child is larger
+        //than the parent then you have to swap them
+        swap(i,largest);
+        //After swapping them check if the child node is still a min heap
+        maxHeapify(largest);
     }
+    
     return;
 }
 
-void buildHeap(std::vector<int>& arr,int n){
+void MaxHeap::buildMaxHeap(){
+    size = arr.size();
    //root of a heap is the first element i=0
    //left child is 2*i+1 and the right child is 2*i+2
    //for i from n/2 till 0 run  heapify (arr, i )
-   //While calling heapify i is the root of the nearly complete binary tree to max heaped
-   int i=n/2;
-   while(i>=0){
-    heapify(arr,i,n);
-    i--;
-   }
+   //While calling heapify i is the root of the nearly complete binary tree to min heaped
+    for(int i=size/2-1;i>=0;i--) 
+           maxHeapify(i);
+}
+
+MaxHeap::MaxHeap(){
+    size = 0;
+}
+
+
+void MaxHeap::printHeap() const{
+    if(isEmpty()){std::cout<<"\nHeap is empty\n";return;}
+    std::cout<<"\nHeap is\n";
+    for(int i=0;i<size;i++){
+        std::cout<<arr[i]<<' ';
+    }
+    std::cout<<'\n';
+}
+
+void MaxHeap::insert(int& a){
+    arr.push_back(a);
+    size++;
+    buildMaxHeap();
+}
+
+const bool MaxHeap::isEmpty() const{
+    if(size == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int MaxHeap::findMax() const{
+    return arr[0];
+}
+
+int MaxHeap::extractMax(){
+    int a = arr[0];
+    swap(0,--size);
+    arr.pop_back();
+    maxHeapify(0);
+    return a;
 }
 
 int main(){
-    std::vector<int> numbers({1,7,6,3,5,3,6});
-    buildHeap(numbers,numbers.size()-1);
-    for(int i=0;i<numbers.size();i++){
-        std::cout<<numbers[i]<<' ';
-    }
+    MaxHeap heap;
+    heap.insert(1);
+    heap.insert(3);
+    heap.insert(5);
+    int max = heap.extractMax();
+    heap.printHeap();
     std::cout<<'\n';
     return 0;
 }
