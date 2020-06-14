2fe254681f6735e7790921dd7336ff97bb2ae695
Rohit Karunakaran <rohitkarun@gmail.com>
2020-06-14 10:28:20 +0530

    On branch mergesort  Your branch is up to date with 'origin/mergesort'.
     Changes to be committed:
    	modified:   ../Algorithms/SortingAlgo/Mergesort.cpp
    
     Untracked files:
    	../Algorithms/SortingAlgo/hs.cpp

--- Algorithms/SortingAlgo/Mergesort.cpp
+++ Algorithms/SortingAlgo/Mergesort.cpp
@@ -2,30 +2,30 @@
 #include<vector>
 int ITERCOUNT = 0;
 void fingerThing(std::vector<int>& arr,int last,int begin,int mid);
-void mergeSort(std::vector<int>& arr,int begin, int last){ 
+void mergeSort(std::vector<int>& arr,int begin, int last,bool printArray = false){ 
     //std::cout<<"In merge sort"<<'\n';
     int mid; 
     mid = (begin+last)/2;
     if(begin<last){
-      //mid = (begin+last)/2;
-      mergeSort(arr,begin,mid);
-      mergeSort(arr,mid+1,last);
+      mergeSort(arr,begin,mid,printArray);
+      mergeSort(arr,mid+1,last,printArray);
       fingerThing(arr,begin,mid,last);
     } 
     ITERCOUNT++;
-    std::cout<<"Iteration "<<ITERCOUNT;
-    std::cout<<"\n----------------------------\n";
-    std::cout<<"Value of begin = "<<begin<<'\n';
-    std::cout<<"Value of last = "<<last<<'\n';
-    std::cout<<"Value of mid = "<<mid<<'\n';
-    for(int i = begin;i<=last;i++){
-        std::cout<<arr[i]<<' ';
+    if(printArray){
+        std::cout<<"Iteration "<<ITERCOUNT;
+        std::cout<<"\n----------------------------\n";
+        std::cout<<"Value of begin = "<<begin<<'\n';
+        std::cout<<"Value of last = "<<last<<'\n';
+        std::cout<<"Value of mid = "<<mid<<'\n';
+        for(int i = begin;i<=last;i++){
+            std::cout<<arr[i]<<' ';
+        }
+        std::cout<<"\n\n";
     }
-    std::cout<<"\n\n";
     return;
 }
 void fingerThing(std::vector<int>& arr ,int begin,int mid,int last){
-    //std::cout<<"In pointer sort \n";
     if(last==begin){
         return; 
     }
@@ -43,25 +43,32 @@ void fingerThing(std::vector<int>& arr ,int begin,int mid,int last){
     }
     while(b<=mid){temp.push_back(arr[b++]);}
     while(c<=last){temp.push_back(arr[c++]);}
-    for(int i = 0;i<temp.size();i++){
-        arr[begin+i] = temp[i];
-    }
-    std::cout<<"Changed "<<begin+temp.size()-1<<" elements\n";
-    
     return;
 }
 
 
 int main(){
-    std::vector<int> spy({7,4,3,8,0,2,4,3,2});   
-    for(auto it=spy.begin();it!=spy.end();++it){
-        std::cout<<*it<<' '; 
-    }
+    std::vector<int> spy;   
+    int n;
+    int a;
+    bool printArray;
+    char input;
+    std::cout<<"Welcome to Megre sort implementation\n";
+    std::cout<<"Enter the number of elements in the array\n";
+    std::cin>>n;
+    std::cout<<"Enter the elements in the array\n";    
+    a = readE
     std::cout<<'\n';
-   mergeSort(spy,0,spy.size()-1);
+    std::cout<<"Do you want to print each iteration of the Merge Sort? (y/n) ";
+    std::cin>>input;
     for(auto it=spy.begin();it!=spy.end();++it){
-        std::cout<<*it<<' '; 
+        std::cout<<*it<<' ';
     }
-    std::cout<<"\nTotal of "<<ITERCOUNT<<" iterations taken \n";
+    if(input == 'y'|| input == 'Y')printArray = true;
+    else printArray = false;
+   mergeSort(spy,0,spy.size()-1,printArray);
+    for(auto it=spy.begin();it!=spy.end();++it){
+        std::cout<<*it<<' ';
+    }
     return 0;
     }
