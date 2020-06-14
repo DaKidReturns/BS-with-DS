29c50225fe7fcb71956484a30322a58647e41d4c
Rohit Karunakaran <rohitkarun@gmail.com>
2020-06-14 10:24:02 +0530

    On branch bst  Your branch is up to date with 'origin/bst'.
     Changes to be committed:
    	deleted:    ../Algorithms/SortingAlgo/Mergesort.cpp
            modified:   BST.cpp
    
     Untracked files:
    	../Algorithms/SortingAlgo/Mergesort.cpp
            ../Algorithms/SortingAlgo/hs.cpp

--- BinarySearchTrees/BST.cpp
+++ BinarySearchTrees/BST.cpp
@@ -8,10 +8,31 @@ class Node{
         Node *rightChild;
         int value;
     public:
+        Node(const int& a);
+        Node(); 
         //Getters
         int getValue() const;
         Node& getLeftChild() const;
         Node& getRightChild() const;
         Node& getParent() const;
         //Setters
+        void setValue(const int& );
+        void setParent(Node* );
+        void setRightChild(setRightChildNode* );
+        void setLeftChild(Node* );
 }
+
+Node::Node(const int& a){
+    this->value = a;
+    this->leftChild=NULL;
+    this->rightChild=NULL;
+    this->parent=NULL;
+}
+
+Node::Node(){
+    this->leftChild=NULL;
+    this->rightChild=NULL;
+    this->parent=NULL;
+}
+
+
