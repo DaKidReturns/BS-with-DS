3fd77e0e1f8d7ace79c4dc1eed87b7f46eeb7239
Rohit Karunakaran <rohitkarun@gmail.com>
2020-06-18 19:13:18 +0530

    Added reversing for a circular linked list and also a print method

--- LinkedList/CircularLinkedList.cpp
+++ LinkedList/CircularLinkedList.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<unistd.h>
 
 class CNode{
 	private:
@@ -20,6 +21,8 @@ class CLinkedList{
 		void advance();
 		void add(const std::string& );
 		void remove();
+        void reverse();
+        void printList();
 };
 
 CLinkedList::CLinkedList(){
@@ -29,7 +32,6 @@ CLinkedList::CLinkedList(){
 CLinkedList::~CLinkedList(){
 	while(!empty())
 		remove();
-
 }
 
 bool CLinkedList::empty() const{
@@ -45,25 +47,59 @@ const std::string& CLinkedList::back() const{
 }
 
 void CLinkedList::advance(){
-	cursor =cursor->next;
+	cursor=cursor->next;
 }
 
 void CLinkedList::add(const std::string& e){
 	CNode* n = new CNode;
 	n->elem = e;
-	if(cursor!= NULL){
+	if(cursor== NULL){
 		n->next = n;
-		cursor = n;
 	}
 	else{
-		n->next = cursor->next;
+		n->next=cursor->next;
 		cursor->next = n;
 	}
+	cursor = n;
 }
 
 void CLinkedList::remove(){
-	CNode* old = cursor->next;
-	if(old == cursor) cursor = NULL;
-	else cursor->next = old->next;
-	delete old;
+	if(!(this->empty())){
+        CNode* old = cursor->next;
+	    if(old == cursor) cursor = NULL;
+	    else cursor->next = old->next;
+	    delete old;
+    }
+}
+
+void CLinkedList::reverse(){ //Uses high space
+    if(this->empty()||cursor->next==cursor){
+        return;
+    }
+    CNode* current=cursor->next;
+    CNode* prev;
+    CNode* nextNode=current->next;
+    while(current!=cursor){
+        prev = current;
+        current = nextNode;
+        nextNode = current->next;
+        current->next = prev;
+    }
+    prev=current;
+    current = nextNode;
+    nextNode = current->next;
+    current->next = prev;
+    cursor = current;
+    
+}
+
+void CLinkedList::printList(){
+    CNode* start=cursor;
+    this->advance();
+    while(cursor!=start){
+        std::cout<<cursor->elem<<" ";
+        this->advance();
+   }
+    std::cout<<cursor->elem<<" ";
+    std::cout<<std::endl;
 }
